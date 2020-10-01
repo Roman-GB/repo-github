e63385095e9038e4c55144c46e9e1b0cbc019279
Roman <Chizhikov.85@inbox.ru>
2020-10-02 07:20:37 +1000

    Сделал Lesson-1 без 4 пункта

---
+++ Lesson1/1.py
@@ -0,0 +1,7 @@
+a = int(input('Введите число'))
+c = int(input('Введите число'))
+b = str(input('Введите текст'))
+print(a)
+print(b)
+print(c)
+
---
+++ Lesson1/3.py
@@ -0,0 +1,8 @@
+t = str(input('Введите число: '))
+k = int(t)
+x = int(t + t)
+y = int(t + t + t)
+c = int(k + x + y)
+print(c)
+
+
